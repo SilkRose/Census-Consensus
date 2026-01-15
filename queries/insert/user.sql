7faebc8ddeda75e975fa1c692674a559c1d45ef5
Silk Rose <silkrose@love-tolerance.com>
2026-01-14 19:57:33 -0500

    Fix insert user SQL statement

--- queries/insert/user.sql
+++ queries/insert/user.sql
@@ -1,11 +1,10 @@
 INSERT INTO Users
-	(id, name, pfp_url, type, date_cached)
+	(id, name, pfp_url, type)
 VALUES
-	($1, $2, $3, $4, $5)
+	($1, $2, $3, $4)
 ON CONFLICT(id) DO UPDATE SET
 	name = EXCLUDED.name,
 	pfp_url = EXCLUDED.pfp_url,
-	type = EXCLUDED.type,
-	date_cached = now()
+	type = EXCLUDED.type
 RETURNING
-	id, name, pfp_url, type AS "user_type: UserType", feedback_private, feedback_public, date_cached;
+	id, name, pfp_url, type AS "user_type: UserType", feedback_private, feedback_public, date_joined;
