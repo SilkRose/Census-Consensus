0fcdd0f22e58e9a8de356d1a2997e8d94a4d1cea
Meadow Liu <meadowsys@kiwin.gay>
2026-01-16 15:05:23 -0800

    run which for pnpm, and improve debuggability of build script
    ~sf

--- build.rs
+++ build.rs
@@ -11,11 +11,14 @@ fn main() {
 	let mut node_modules = env::current_dir().unwrap();
 	node_modules.push("node_modules");
 
+	let pnpm_path = run_command("which", ["pnpm"])();
+	let pnpm_path = pnpm_path.trim();
+
 	if !fs::exists(&*node_modules).unwrap() {
-		run_command("pnpm", ["install"]);
+		let _ = run_command(pnpm_path, ["install"]);
 	}
 
-	run_command("pnpm", [
+	let _ = run_command(pnpm_path, [
 		"tailwindcss",
 		"--input",
 		"src/css.css",
@@ -25,33 +28,46 @@ fn main() {
 
 }
 
-fn run_command<'h>(command: &str, args: impl IntoIterator<Item = &'h str> + Clone) {
-	let mut child = Command::new(command)
+fn run_command<'h>(command: &str, args: impl IntoIterator<Item = &'h str> + Clone) -> impl FnOnce() -> String {
+	let child = Command::new(command)
 		.args(args.clone())
 		.stdin(Stdio::null())
 		.stdout(Stdio::piped())
 		.stderr(Stdio::piped())
-		.spawn()
-		.unwrap();
+		.spawn();
 
-	let mut stdout_stream = child.stdout.take().unwrap();
-	let mut stderr_stream = child.stderr.take().unwrap();
-
-	let result = child.wait().unwrap();
-
-	if !result.success() {
+	let whole_command = || {
 		let mut whole_command = String::new();
 
 		whole_command.push_str(command);
 		whole_command.extend(args.into_iter().flat_map(|item| [" ", item]));
 
-		let mut stdout = Vec::new();
-		stdout_stream.read_to_end(&mut stdout).unwrap();
+		whole_command
+	};
+
+	let mut child = match child {
+		Ok(child) => { child }
+		Err(err) => {
+			eprintln!("running command wasn't successful");
+			eprintln!("command: {}", whole_command());
+			eprintln!("error: {err}");
+			panic!()
+		}
+	};
+
+	let mut stdout_stream = child.stdout.take().unwrap();
+	let mut stderr_stream = child.stderr.take().unwrap();
+
+	let result = child.wait().unwrap();
+	let mut stdout = Vec::new();
+	stdout_stream.read_to_end(&mut stdout).unwrap();
+
+	if !result.success() {
 		let mut stderr = Vec::new();
 		stderr_stream.read_to_end(&mut stderr).unwrap();
 
 		eprintln!("running command wasn't successful");
-		eprintln!("command: {whole_command}");
+		eprintln!("command: {}", whole_command());
 
 		eprintln!("--- command stdout\n");
 		io::stderr().write_all(&stdout).unwrap();
@@ -63,4 +79,8 @@ fn run_command<'h>(command: &str, args: impl IntoIterator<Item = &'h str> + Clon
 
 		panic!();
 	}
+
+	// this is so we don't validate stdout is valid utf-8
+	// unless we _actually_ need it to be
+	move || String::from_utf8(stdout).unwrap()
 }
