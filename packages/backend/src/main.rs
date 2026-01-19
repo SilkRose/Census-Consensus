93e034028dc624ea0f385cc7f9121c02c0f64c54
Meadow Liu <meadowsys@kiwin.gay>
2026-01-19 15:26:12 -0800

    move rest of the code to backend crate
    ~sf

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,3 +1,14 @@
+pub use self::auth::fimfic_auth;
+pub use self::db::Db;
+pub use self::fimfic_cfg::FimficCfg;
+pub use self::http::HttpClient;
+
+pub use actix_files::Files;
+pub use actix_web::{ App as ActixApp, HttpServer };
+pub use actix_web::middleware::Compress;
+pub use actix_web::web::Data;
+pub use anyhow::Result;
+
 mod auth;
 mod db;
 mod env_vars;
@@ -6,4 +17,52 @@ mod http;
 mod rand;
 mod structs;
 
-fn main() {}
+fn main() -> Result<()> {
+	// SAFETY: we do this before doing anything else in the program, including
+	// before creating an actix runtime, so this should be fine :3
+	unsafe { env_vars::set_required_vars() }
+
+	async_main()
+}
+
+#[actix_web::main]
+async fn async_main() -> Result<()> {
+	env_vars::load_dotenv();
+	env_vars::check();
+
+	let db = Db::new(&env_vars::database_url()).await?;
+	let db = Data::new(db);
+
+	let client_id = env_vars::fimfic_client_id();
+	let oauth_redirect_url = env_vars::fimfic_oauth_redirect_url();
+	let login_url = fimfic_cfg::make_login_url(&client_id, &oauth_redirect_url);
+	let fimfic_cfg = FimficCfg {
+		client_id,
+		client_secret: env_vars::fimfic_client_secret(),
+		oauth_redirect_url,
+		login_url
+	};
+	let fimfic = Data::new(fimfic_cfg);
+
+	let http_client = HttpClient::new()?;
+	let http_client = Data::new(http_client);
+
+	println!("listening on 127.0.0.1:3000");
+
+	let server = HttpServer::new(move || {
+		ActixApp::new()
+			.service(Files::new("/", "./target/site"))
+			.service(fimfic_auth)
+			.app_data(db.clone())
+			.app_data(fimfic.clone())
+			.app_data(http_client.clone())
+			.wrap(Compress::default())
+	});
+
+	server
+		.bind("127.0.0.1:3000")?
+		.run()
+		.await?;
+
+	Ok(())
+}
