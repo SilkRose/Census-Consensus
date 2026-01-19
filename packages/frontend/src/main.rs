7ae3ae8dbb7b859ede6d1762f1d317674fdf055c
Meadow Liu <meadowsys@kiwin.gay>
2026-01-19 15:40:50 -0800

    remove accidentally forgotten about remant of SSR
    ~sf

--- packages/frontend/src/main.rs
+++ packages/frontend/src/main.rs
@@ -50,14 +50,6 @@ fn HomePage() -> impl IntoView {
 
 #[component]
 fn NotFound() -> impl IntoView {
-	#[cfg(feature = "ssr")] {
-		use actix_web::http::StatusCode;
-		use leptos_actix::ResponseOptions;
-
-		let resp = expect_context::<ResponseOptions>();
-		resp.set_status(StatusCode::NOT_FOUND);
-	}
-
 	mview! {
 		h1 { "Not Found" }
 	}
