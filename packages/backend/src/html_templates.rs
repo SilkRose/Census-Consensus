529ad7e258cbeba3a46b3ec0d8b2098d335108b4
Silk Rose <silkrose@love-tolerance.com>
2026-01-24 15:44:14 -0500

    Fix feedback page value position

--- packages/backend/src/html_templates.rs
+++ packages/backend/src/html_templates.rs
@@ -9,14 +9,14 @@ pub fn form_html_template(
 				form method = "post" action = "/user-feedback" {
 					label for = "public" { h2  { "Public Feedback" } } br;
 					@if let Some(public_feedback) = public_feedback {
-							textarea id = "public" type = "text" name = "feedback_public" cols = "30" rows = "10" value = (public_feedback) {}
+							textarea id = "public" type = "text" name = "feedback_public" cols = "30" rows = "10" { (public_feedback) }
 					} @else {
 						textarea id = "public" type = "text" name = "feedback_public" cols = "30" rows = "10" {}
 					}
 					br;
 					label for = "private" { h2  { "Private Feedback" } } br;
 					@if let Some(private_feedback) = private_feedback {
-							textarea id = "private" type = "text" name = "feedback_private" cols = "30" rows = "10" value = (private_feedback) {}
+							textarea id = "private" type = "text" name = "feedback_private" cols = "30" rows = "10" { (private_feedback) }
 					} @else {
 						textarea id = "private" type = "text" name = "feedback_private" cols = "30" rows = "10" {}
 					}
