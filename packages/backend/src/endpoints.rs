8b7c9f30fbcf21a247db62f0d4a42243bd7873f0
Meadow Liu <meadowsys@kiwin.gay>
2026-01-24 16:04:39 -0800

    use SessionInfo extractor
    ~sf

--- packages/backend/src/endpoints.rs
+++ packages/backend/src/endpoints.rs
@@ -1,3 +1,4 @@
+use crate::auth::SessionInfo;
 use crate::error::ErrorWrapper;
 use crate::utility::redirect;
 use crate::{Db, html_templates::form_html_template};
@@ -7,33 +8,24 @@ use std::collections::HashMap;
 
 #[get("/user-feedback")]
 pub async fn get_user_feedback(
-	req: HttpRequest, db: ThinData<Db>,
+	db: ThinData<Db>, session: SessionInfo,
 ) -> actix_web::Result<impl Responder> {
-	if let Some(token) = req.cookie("fimfic-auth-session") {
-		let session = db
-			.get_session_by_token(token.value())
-			.await
-			.map_err(ErrorWrapper)?;
-		if let Some(session) = session {
-			let user = db
-				.get_user(session.user_id)
-				.await
-				.map_err(ErrorWrapper)?
-				.expect(
-					"Database constraints mean a user will always be present if they have a session.",
-				);
-			let page = form_html_template(user.feedback_private, user.feedback_public);
-			return Ok(HttpResponse::Ok()
-				.content_type("text/html; charset=utf-8")
-				.body(page));
-		};
-	}
-	Ok(HttpResponse::Unauthorized().finish())
+	let user = db
+		.get_user(session.user_id)
+		.await
+		.map_err(ErrorWrapper)?
+		.expect(
+			"Database constraints mean a user will always be present if they have a session.",
+		);
+	let page = form_html_template(user.feedback_private, user.feedback_public);
+	Ok(HttpResponse::Ok()
+		.content_type("text/html; charset=utf-8")
+		.body(page))
 }
 
 #[post("/user-feedback")]
 pub async fn set_user_feedback(
-	req: HttpRequest, body: String, db: ThinData<Db>,
+	req: HttpRequest, body: String, db: ThinData<Db>, session: SessionInfo,
 ) -> actix_web::Result<impl Responder> {
 	let feedback = serde_urlencoded::from_str::<HashMap<String, String>>(&body)?;
 	let private_feedback = feedback
@@ -45,27 +37,9 @@ pub async fn set_user_feedback(
 		.and_then(|msg| if msg.is_empty() { None } else { Some(msg) })
 		.cloned();
 
-	if let Some(token) = req.cookie("fimfic-auth-session") {
-		let session = db
-			.get_session_by_token(token.value())
-			.await
-			.map_err(ErrorWrapper)?;
-		if let Some(session) = session {
-			// If the database constraints mean a user is always present,
-			// do we even need to get them before updating their feedback?
-			//
-			// let user = db
-			// 	.get_user(session.user_id)
-			// 	.await
-			// 	.map_err(ErrorWrapper)?
-			// 	.expect(
-			// 		"Database constraints mean a user will always be present if they have a session.",
-			// 	);
-			db.update_user_feedback(session.user_id, private_feedback, public_feedback)
-				.await
-				.map_err(ErrorWrapper)?;
-		}
-	}
+	db.update_user_feedback(session.user_id, private_feedback, public_feedback)
+		.await
+		.map_err(ErrorWrapper)?;
 
 	Ok(HttpResponse::SeeOther()
 		.append_header(("Location", redirect(req)))
