349e4ad411cf3bb642a6c0a0c316dae5c6b894fa
Silk Rose <silkrose@love-tolerance.com>
2026-01-24 19:56:02 -0500

    Add a lax length of 1 million to user feedback

--- packages/backend/src/html_templates.rs
+++ packages/backend/src/html_templates.rs
@@ -10,17 +10,17 @@ pub fn user_feedback_html(
 					label for = "public" { h3  { "Public Feedback" } } br;
 					p style = "opacity: 80%" { "May appear in a future blog post about this event." }
 					@if let Some(public_feedback) = public_feedback {
-							textarea id = "public" type = "text" name = "feedback_public" cols = "30" rows = "10" { (public_feedback) }
+							textarea id = "public" type = "text" name = "feedback_public" maxlength = "1000000" cols = "30" rows = "10" { (public_feedback) }
 					} @else {
-						textarea id = "public" type = "text" name = "feedback_public" cols = "30" rows = "10" {}
+						textarea id = "public" type = "text" name = "feedback_public" maxlength = "1000000" cols = "30" rows = "10" {}
 					}
 					br;
 					label for = "private" { h3  { "Private Feedback" } } br;
 					p style = "opacity: 80%" { "Shared only with the developers and writers of this event." }
 					@if let Some(private_feedback) = private_feedback {
-							textarea id = "private" type = "text" name = "feedback_private" cols = "30" rows = "10" { (private_feedback) }
+							textarea id = "private" type = "text" name = "feedback_private" maxlength = "1000000" cols = "30" rows = "10" { (private_feedback) }
 					} @else {
-						textarea id = "private" type = "text" name = "feedback_private" cols = "30" rows = "10" {}
+						textarea id = "private" type = "text" name = "feedback_private" maxlength = "1000000" cols = "30" rows = "10" {}
 					}
 					br;
 					button type = "submit" { "Submit Feedback" }
