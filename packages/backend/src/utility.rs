c0ebf7ff60cbfd25ba91180a6ccb3cb6fc4bed9a
Silk Rose <silkrose@love-tolerance.com>
2026-01-28 23:12:38 -0500

    Add count words function

--- packages/backend/src/utility.rs
+++ packages/backend/src/utility.rs
@@ -1,4 +1,5 @@
 use actix_web::HttpRequest;
+use pony::word_stats::word_count;
 
 pub fn redirect(req: HttpRequest) -> String {
 	req.headers()
@@ -6,4 +7,12 @@ pub fn redirect(req: HttpRequest) -> String {
 		.and_then(|v| v.to_str().ok())
 		.unwrap_or("/")
 		.into()
+}
+
+pub fn count_words(text: &str) -> usize {
+	let count = word_count(text);
+	match count {
+		Ok(count) => count,
+		Err(_) => text.split_whitespace().count(),
+	}
 }
