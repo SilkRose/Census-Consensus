b896c0277a42e9be18698fdd6f0d0997b77c47d4
Silk Rose <silkrose@love-tolerance.com>
2026-03-24 00:10:12 -0400

    Work on event loop control

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use chrono::Utc;
+use pony::markdown::WarningType;
+use pony::markdown::bbcode::parse;
 use serde_json::{Value, json};
 use std::collections::HashMap;
 use std::time::Duration;
 
 use crate::endpoints::*;
 use crate::structs::UserType;
+use crate::utility::parse_options;
 
 pub use self::database::*;
 pub use self::error::Result;
@@ -203,15 +206,33 @@ async fn event_control_tick(
 		.map_or(chapter.vote_duration, |m| m - 1);
 	db.update_chapter_minutes_left(chapter.id, Some(minutes_left))
 		.await?;
-	if minutes_left <= 0 {
-		let question_count = db.get_question_count_by_chapter(chapter.id).await?;
-		if question_count > 0 {
-			// normal chapter
-		} else {
-			// final chapter
-		}
+	let publish = minutes_left <= 0;
+	let final_chapter = db.get_question_count_by_chapter(chapter.id).await? <= 0;
+	if publish {
+		let data = db.get_latest_chapter_revision(chapter.id).await?;
+		let json = match final_chapter {
+			true => chapter_json(&data.title, &data.outro_text.ok_or("Missing outro!")?, None),
+			false => {
+				let mut texts = Vec::new();
+				if let Some(ref intro) = data.intro_text {
+					texts.push(parse(intro.trim(), &WarningType::Quiet));
+				}
+				let questions = db.get_questions_by_chapter(chapter.id).await?;
+				for question in questions {
+					let data = db.get_latest_question_revision(question.id).await?;
+					let options = data.option_writing.ok_or("Missing options!")?;
+					let options = parse_options(&options, &data.question_type);
+					let votes = db.get_all_votes_by_question(question.id).await?;
+					// insert parsing results here
+					texts.push(parse("".trim(), &WarningType::Quiet));
+				}
+				if let Some(ref outro) = data.outro_text {
+					texts.push(parse(outro.trim(), &WarningType::Quiet));
+				}
+				chapter_json(&data.title, &texts.join("\n\n"), None)
+			}
+		};
 	}
-	// update story
 	Ok(())
 }
 
